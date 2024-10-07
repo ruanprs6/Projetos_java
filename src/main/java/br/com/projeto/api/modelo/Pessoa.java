0764b5f75900983dadbd0e9647f56a456a0e4b69
ruanprs6 <ruanprs6@outlook.com>
2024-10-07 08:25:06 -0300

    teste

--- src/main/java/br/com/projeto/api/modelo/Pessoa.java
+++ src/main/java/br/com/projeto/api/modelo/Pessoa.java
@@ -1,20 +1,48 @@
 package br.com.projeto.api.modelo;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+
+
+/*import jakarta.annotation.Generated; */
+@Entity
+@Table(name = "peassoas")
 public class Pessoa {
-    //Atributos
-private String nome;
-public String getNome() {
-    return nome;
-}
-public void setNome(String nome) {
-    this.nome = nome;
-}
-private int idade;
-public int getIdade() {
-    return idade;
-}
-public void setIdade(int idade) {
-    this.idade = idade;
-}
+
+    // Atributos
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int codigo;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    private int idade;
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
 
 }
---
+++ src/main/java/br/com/projeto/api/repositorio/Repositorio.java
@@ -0,0 +1,8 @@
+package br.com.projeto.api.repositorio;
+
+/*import org.springframework.stereotype.Repository;
+
+/*@Repository
+public interface Repositorio extends CrudRepository<Pessoa, integer> {
+    
+}*/
