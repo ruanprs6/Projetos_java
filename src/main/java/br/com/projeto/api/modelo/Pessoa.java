39ddd461aded834c2362605660d7d58e583d25ac
ruanprs6 <ruanprs6@outlook.com>
2024-10-08 15:37:29 -0300

    teste conexão mysql

--- src/main/java/br/com/projeto/api/modelo/Pessoa.java
+++ src/main/java/br/com/projeto/api/modelo/Pessoa.java
@@ -1,14 +1,16 @@
 package br.com.projeto.api.modelo;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
 
-
-
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//import javax.persistence.Table;
+//import jakarta.persistence.Entity;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
 /*import jakarta.annotation.Generated; */
-@Entity
+
 @Table(name = "peassoas")
 public class Pessoa {
 
