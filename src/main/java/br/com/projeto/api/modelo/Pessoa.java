f43e22e225d3b1b0f8a0f8d60dee595db6c2d87e
ruanprs6 <ruanprs6@outlook.com>
2024-10-08 15:41:54 -0300

    criar tabela mysql

--- src/main/java/br/com/projeto/api/modelo/Pessoa.java
+++ src/main/java/br/com/projeto/api/modelo/Pessoa.java
@@ -4,13 +4,13 @@ package br.com.projeto.api.modelo;
 //import javax.persistence.GenerationType;
 //import javax.persistence.Id;
 //import javax.persistence.Table;
-//import jakarta.persistence.Entity;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.GeneratedValue;
 /*import jakarta.annotation.Generated; */
-
+@Entity
 @Table(name = "peassoas")
 public class Pessoa {
 
