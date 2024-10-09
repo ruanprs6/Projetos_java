b05c90e637fe6c70099584d67e9101a674d3050e
ruanprs6 <ruanprs6@outlook.com>
2024-10-09 13:54:44 -0300

    atualizar tabela

--- src/main/java/br/com/projeto/api/controle/Controle.java
+++ src/main/java/br/com/projeto/api/controle/Controle.java
@@ -1,12 +1,17 @@
 package br.com.projeto.api.controle;
 
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.projeto.api.modelo.Pessoa;
+import br.com.projeto.api.repositorio.Repositorio;
+
 import org.springframework.web.bind.annotation.PostMapping;
 
 
@@ -16,6 +21,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 
 public class Controle {
+   @Autowired
+   private Repositorio acao;
+
+
+   @PostMapping("/api")
+   public Pessoa cadastrar(@RequestBody Pessoa obj){
+      return acao.save(obj);
+   }
+
+   @GetMapping("/api")
+   public List<Pessoa> selecionar(){
+      return acao.findAll();
+   }
+
     @GetMapping("")
      public String mensagem(){
          return "Hello word!";
--- src/main/java/br/com/projeto/api/repositorio/Repositorio.java
+++ src/main/java/br/com/projeto/api/repositorio/Repositorio.java
@@ -1,8 +1,15 @@
 package br.com.projeto.api.repositorio;
 
-/*import org.springframework.stereotype.Repository;
+import java.util.List;
 
-/*@Repository
-public interface Repositorio extends CrudRepository<Pessoa, integer> {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.projeto.api.modelo.Pessoa;
+
+@Repository
+public interface Repositorio extends CrudRepository<Pessoa, Integer> {
     
-}*/
+    List<Pessoa> findAll();
+
+}
